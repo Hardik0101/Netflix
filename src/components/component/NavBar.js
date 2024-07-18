fc00b597e387a7739a36e081fafd2273ebe7450e
Hardik0101 <hardik.prajapati@seaflux.tech>
2024-07-18 12:51:45 +0530

    fix: navbar components

--- src/components/component/NavBar.js
+++ src/components/component/NavBar.js
@@ -3,20 +3,11 @@ import { Input } from "../ui/input";
 import { FiSearch } from "react-icons/fi";
 import Image from "next/image";
 import { IoHome } from "react-icons/io5";
-
 import { FaRegCircleUser } from "react-icons/fa6";
 import { Button } from "../ui/button";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
+import { usePathname, useRouter } from "next/navigation";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   Dialog,
   DialogContent,
@@ -25,22 +16,12 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { usePathname, useRouter } from "next/navigation";
-
-import Link from "next/link";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
-import { Label } from "../ui/label";
 import { HiMenu } from "react-icons/hi";
 import { FolderHeart, CircleX } from "lucide-react";
+import TooltipMessage from "./Tooltip";
+import Alert from "./Alert";
+import UserForm from "./UserForm";
+import SheetMenu from "./SheetMenu";
 
 function NavBar({ searchText, setSearchText }) {
   const router = useRouter();
@@ -48,6 +29,12 @@ function NavBar({ searchText, setSearchText }) {
 
   const handleInputChange = (text) => {
     setSearchText(text);
+  };
+
+  const initialValues = {
+    email: "test@example.com",
+    name: "",
+    mobile: "",
   };
 
   const pathname = usePathname();
@@ -85,20 +72,20 @@ function NavBar({ searchText, setSearchText }) {
         </div>
       </div>
       <div className="block md:hidden">
-        <Sheet>
-          <SheetTrigger asChild>
+        <SheetMenu
+          triggerBtn={
             <HiMenu className="text-black cursor-pointer" size={30} />
-          </SheetTrigger>
-          <SheetContent className="flex justify-center">
-            <SheetHeader className="flex flex-col ">
-              <SheetClose asChild>
-                <Link href={"/"}>
-                  <div className="flex flex-row gap-x-1 items-center  rounded-sm p-1 hover:bg-red-200">
-                    <IoHome color="black" size={16} />
-                    <h1>Home</h1>
-                  </div>
-                </Link>
-              </SheetClose>
+          }
+          closeSheet={
+            <Link href={"/"}>
+              <div className="flex flex-row gap-x-1 items-center  rounded-sm p-1 hover:bg-red-200">
+                <IoHome color="black" size={16} />
+                <h1>Home</h1>
+              </div>
+            </Link>
+          }
+          content={
+            <>
               <Dialog>
                 <DialogTrigger asChild>
                   <div className="flex flex-row gap-x-1 items-center rounded-sm p-1 hover:bg-red-200 cursor-pointer">
@@ -115,28 +102,7 @@ function NavBar({ searchText, setSearchText }) {
                     </DialogDescription>
                   </DialogHeader>
                   <div>
-                    <form className="flex flex-col gap-y-2 mt-4">
-                      <Label className="text-black">Email address</Label>
-                      <Input
-                        type="email"
-                        className="border-gray-400"
-                        value={"test@example.com"}
-                        disabled
-                        placeholder="Enter email address"
-                      />
-                      <Label className="text-black">Name</Label>
-                      <Input
-                        type="text"
-                        className="border-gray-400"
-                        placeholder="Enter Your Name"
-                      />
-                      <Label className="text-black">Mobile Number</Label>
-                      <Input
-                        type="phone"
-                        className="border-gray-400"
-                        placeholder="Enter Your Mobile Number"
-                      />
-                    </form>
+                    <UserForm initialValues={initialValues} />
                     <Button
                       className="mt-3 bg-[#e50914] hover:bg-red-700"
                       type="submit"
@@ -156,151 +122,114 @@ function NavBar({ searchText, setSearchText }) {
                 <FolderHeart color="black" size={16} />
                 <h1>My List</h1>
               </div>
-              <AlertDialog>
-                <AlertDialogTrigger asChild>
-                  <Button className="bg-[#e50914] hover:bg-red-700 h-[30px]">
-                    Log out
-                  </Button>
-                </AlertDialogTrigger>
-                <AlertDialogContent>
-                  <AlertDialogHeader>
-                    <AlertDialogTitle>
-                      Are you absolutely sure?
-                    </AlertDialogTitle>
-                    <AlertDialogDescription>
-                      Are you sure you want to log out? Click 'OK' to confirm
-                      and logout, or 'Cancel' to stay logged in.
-                    </AlertDialogDescription>
-                  </AlertDialogHeader>
-                  <AlertDialogFooter>
-                    <AlertDialogCancel>Cancel</AlertDialogCancel>
-                    <AlertDialogAction
-                      className="bg-[#e50914] hover:bg-red-700"
-                      onClick={() => router.push("/in")}
-                    >
-                      OK
-                    </AlertDialogAction>
-                  </AlertDialogFooter>
-                </AlertDialogContent>
-              </AlertDialog>
-            </SheetHeader>
-          </SheetContent>
-        </Sheet>
+              <Alert
+                btnTitle={"Log out"}
+                alertMessage={"Are you absolutely sure?"}
+                alertDescription={
+                  " Are you sure you want to log out? Click 'OK' to confirm and logout, or 'Cancel' to stay logged in."
+                }
+                confirmBtnTitle={"OK"}
+                cancelBtnTitle={"Cancel"}
+                onConfirm={() => router.push("/in")}
+                btnStyle={"bg-[#e50914] hover:bg-red-700 h-[30px]"}
+              />
+            </>
+          }
+        />
       </div>
       <ul
         className={`max-sm:hidden max-md:hidden md:flex flex-row items-center gap-x-2 md:gap-x-3`}
       >
-        <li
-          className={`p-1 md:p-2 rounded-full ${
-            pathname === "/" ? "bg-white" : "hover:bg-white"
-          } cursor-pointer`}
-        >
-          <Link href={"/"}>
-            <IoHome
-              color="black"
-              size={30}
-              className="max-sm:hidden max-md:hidden"
-            />
-          </Link>
-        </li>
-        <li className="p-1 md:p-2 rounded-full hover:bg-white cursor-pointer">
-          <Sheet>
-            <SheetTrigger asChild>
-              <FaRegCircleUser
+        <TooltipMessage
+          button={
+            <li
+              className={`p-1 md:p-2 rounded-full ${
+                pathname === "/" ? "bg-white" : "hover:bg-white"
+              } cursor-pointer`}
+            >
+              <Link href={"/"}>
+                <IoHome
+                  color="black"
+                  size={30}
+                  className="max-sm:hidden max-md:hidden"
+                />
+              </Link>
+            </li>
+          }
+          message={"Home"}
+        />
+        <TooltipMessage
+          button={
+            <li className="p-1 md:p-2 rounded-full hover:bg-white cursor-pointer">
+              <SheetMenu
+                triggerBtn={
+                  <FaRegCircleUser
+                    color="black"
+                    size={30}
+                    className="max-sm:hidden"
+                  />
+                }
+                title={"Profile"}
+                image={
+                  <Avatar className="w-16 h-16 flex items-center">
+                    <AvatarImage
+                      className="border-black rounded-full border-[2px] p-[1px]"
+                      src="https://github.com/shadcn.png"
+                      alt="profile"
+                    />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                }
+                description={
+                  "  Make changes to your profile here. Click save when you're done."
+                }
+                content={
+                  <div>
+                    <UserForm initialValues={initialValues} />
+                    <Button
+                      className="mt-3 bg-[#e50914] hover:bg-red-700"
+                      type="submit"
+                      onClick={() => setEdit(!edit)}
+                    >
+                      {edit ? "Edit" : "Save changes"}
+                    </Button>
+                  </div>
+                }
+              />
+            </li>
+          }
+          message={"Profile"}
+        />
+        <TooltipMessage
+          button={
+            <li
+              className={`p-1 md:p-2 rounded-full  ${
+                pathname === "/mylist" ? "bg-white" : "hover:bg-white"
+              } cursor-pointer`}
+              onClick={() => {
+                router.push("/mylist");
+              }}
+            >
+              <FolderHeart
                 color="black"
                 size={30}
-                className="max-sm:hidden"
+                className="max-sm:hidden max-md:hidden"
               />
-            </SheetTrigger>
-            <SheetContent>
-              <SheetHeader className="flex flex-col items-center">
-                <SheetTitle>Profile</SheetTitle>
-                <Avatar className="w-16 h-16 flex items-center">
-                  <AvatarImage
-                    className="border-black rounded-full border-[2px] p-[1px]"
-                    src="https://github.com/shadcn.png"
-                    alt="@shadcn"
-                  />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <SheetDescription>
-                  Make changes to your profile here. Click save when you're
-                  done.
-                </SheetDescription>
-              </SheetHeader>
-              <div>
-                {/* Profile details */}
-                <form className="flex flex-col gap-y-2 mt-4">
-                  <Label className="text-black">Email address</Label>
-                  <Input
-                    type="email"
-                    className="border-gray-400"
-                    value={"test@example.com"}
-                    disabled
-                    placeholder="Enter email address"
-                  />
-                  <Label className="text-black">Name</Label>
-                  <Input
-                    type="text"
-                    className="border-gray-400"
-                    placeholder="Enter Your Name"
-                  />
-                  <Label className="text-black">Mobile Number</Label>
-                  <Input
-                    type="phone"
-                    className="border-gray-400"
-                    placeholder="Enter Your Mobile Number"
-                  />
-                </form>
-                <Button
-                  className="mt-3 bg-[#e50914] hover:bg-red-700"
-                  type="submit"
-                  onClick={() => setEdit(!edit)}
-                >
-                  {edit ? "Edit" : "Save changes"}
-                </Button>
-              </div>
-            </SheetContent>
-          </Sheet>
-        </li>
-        <li
-          className={`p-1 md:p-2 rounded-full  ${
-            pathname === "/mylist" ? "bg-white" : "hover:bg-white"
-          } cursor-pointer`}
-          onClick={() => {
-            router.push("/mylist");
-          }}
-        >
-          <FolderHeart
-            color="black"
-            size={30}
-            className="max-sm:hidden max-md:hidden"
-          />
-        </li>
+            </li>
+          }
+          message={"My List"}
+        />
         <li className="p-1 md:p-2">
-          <AlertDialog>
-            <AlertDialogTrigger asChild>
-              <Button>Log out</Button>
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-              <AlertDialogHeader>
-                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                <AlertDialogDescription>
-                  Are you sure you want to log out? Click 'OK' to confirm and
-                  logout, or 'Cancel' to stay logged in.
-                </AlertDialogDescription>
-              </AlertDialogHeader>
-              <AlertDialogFooter>
-                <AlertDialogCancel>Cancel</AlertDialogCancel>
-                <AlertDialogAction
-                  className="bg-[#e50914] hover:bg-red-700"
-                  onClick={() => router.push("/in")}
-                >
-                  OK
-                </AlertDialogAction>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialog>
+          <Alert
+            btnTitle={"Log out"}
+            alertMessage={"Are you absolutely sure?"}
+            alertDescription={
+              " Are you sure you want to log out? Click 'OK' to confirm and logout, or 'Cancel' to stay logged in."
+            }
+            confirmBtnTitle={"OK"}
+            cancelBtnTitle={"Cancel"}
+            onConfirm={() => router.push("/in")}
+          />
         </li>
       </ul>
     </div>
---
+++ src/components/component/Tooltip.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+function TooltipMessage({ button, message }) {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>{button}</TooltipTrigger>
+        <TooltipContent>
+          <p>{message}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
+
+export default TooltipMessage;
